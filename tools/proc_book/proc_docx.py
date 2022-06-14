aa2068d653b5316f1e32ae7c2527a92204a5cbcd
Desktop <huangyea@desktop>
2022-06-15 00:01:08 +0800

    Finished parsing `Three Kingdoms`.
    This book was cropped and converted from a PDF file by Acrobat.
    
    Major updates:
    1. Treat a list paragraph as body text.
    2. Add a few characters to the parsing pattern, with a specially per-character parser to identify the missing chars in it.
    3. Have to manually fixed some parsing error in `.docx` file, including adding missing unbalanced braces, strange indents, etc.

--- tools/proc_book/proc_docx.py
+++ tools/proc_book/proc_docx.py
@@ -8,6 +8,7 @@ from pyparsing import pyparsing_unicode as ppu
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--output_dir', type=str, default=None)
+parser.add_argument('--test_cat', type=str, default=None)
 parser.add_argument('file_name')
 args = parser.parse_args()
 
@@ -58,10 +59,14 @@ def iter_text(paragraphs):
                     yield '{0}{1}{2}'.format('## ', ''.join([p.text for p in g2]), postfix)
                 else:
                     temp = ''
-                    for k3, g3 in itertools.groupby(g2, lambda e: (e.paragraph_format.first_line_indent != None and
-                                                                 int(e.paragraph_format.first_line_indent) > 0)
-                                                                 or (e.paragraph_format.left_indent != None and
-                                                                 int(e.paragraph_format.left_indent) > 0)):
+                    for k3, g3 in itertools.groupby(
+                                    g2, lambda e: (e.paragraph_format.first_line_indent != None and
+                                                int(e.paragraph_format.first_line_indent) > 0)
+                                                or (e.paragraph_format.left_indent != None and
+                                                int(e.paragraph_format.left_indent) > 0 and
+                                                # paragaph may starts with a bullet as a list item
+                                                not e.style.name.startswith('List Paragraph'))
+                                                ):
                         text = ''.join([p.text for p in g3])
                         if k3:
                             temp = text
@@ -84,6 +89,7 @@ def test_cmts_parser(parser):
             六月甲戌，任城王彰薨于京都。【◎《任城王传》注引《魏氏春秋》云：初，彰问玺绶，将有异志，故来朝不即得见。彰忿怒暴薨。◎《陈思王传》注引《魏氏春秋》云：是时待遇诸国法峻。任城王暴薨。诸王怀友于之痛。〖参阅《任城王传》注引《世说新语》。〗◎是任城王之死，实为魏文所害，当时陈王亦几不免。天性凉薄，宜其享国之不永也。】甲申，太尉贾诩薨。太白昼见。【◎《晋书·天文志下》云：黄初四年六月甲申，太白昼见。案刘向《五行纪论》曰：“太白少阴，弱，不得专行，故以己未为界，不得经天而行。经天则昼见，其占为兵丧，为不臣，为更王；强国弱，小国强。”是时孙权受魏爵号，而称兵距守。◎弼按：孙权背魏，在黄初三年，与四年之太白昼见何涉？以是知天文、五行、符瑞诸志，多不足据也。】
 
             # Extra long string
+
             ''')
     res_list = result[1][0][1].as_list()
     print(res_list)
@@ -95,7 +101,7 @@ def test_docx(pars):
     for par in pars:
         pf = par.paragraph_format
         if 1 or (pf.first_line_indent != None and int(pf.first_line_indent) > 0) or (pf.left_indent != None and int(pf.left_indent) > 0):
-            print('{0} {4} -- {1} -- {3} -- {2}'.format(pf.first_line_indent, pf.alignment, par.text[0:10], pf.space_before, pf.left_indent))
+            print('{0} {4} -- {5} -- {1} -- {3} -- {2}'.format(pf.first_line_indent, pf.alignment, par.text[0:10], pf.space_before, pf.left_indent, par.style.name))
 
 try:
     file_head, file_extension = os.path.splitext(args.file_name)
@@ -112,25 +118,38 @@ else:
 
 out_filename = filename + '.Rmd'
 
-content = pp.Combine(pp.Word(pp.printables + ppu.Chinese.printables + '〇 　。，；：、-！￥……（）——？《》〈〉〖〗·“”„’‘◎○,;:/-!?<>~()[]{}#@$%^&*+=_/\\|`\'\'"'), adjacent = False) # Combine() is space sensitive, see: https://stackoverflow.com/a/2940844
+chars = pp.printables + ppu.Chinese.printables + '〇 　。，；：、﹑-！￥……（）―？《》〈〉〖〗■□♀．·“”„’‘◎○,;:/-!?<>~()[]{}#@$%^&*+=_/\\|`\'\'"'
+content = pp.Combine(pp.Word(chars), adjacent = False) # Combine() is space sensitive, see: https://stackoverflow.com/a/2940844
 # content = pp.Combine(pp.Regex(r'[^【】]*')) # Regex() is very time consuming
+per_char = pp.Word(chars + '【】')
 comment = pp.nested_expr('【', '】', content=content)
 cmts_parser = ((content + comment[...]) ^ comment)[...]
 pp.enable_all_warnings()
 
-# test_cmts_parser(cmts_parser); exit(0)
-# test_docx(doc.paragraphs); exit(0)
+if args.test_cat == 'parser':
+    test_cmts_parser(per_char)#cmts_parser)
+    exit(0)
+if args.test_cat == 'docx':
+    test_docx(doc.paragraphs)
+    exit(0)
+    
 
 # Parse nested comments as this post says: https://stackoverflow.com/a/5454510
 styled_texts = []
 FLAG_DEBUG = 0
 for text in iter_text(doc.paragraphs):
+    if args.test_cat == 'char':
+        parser = per_char
+    else:
+        parser = cmts_parser
+
     try:
-        res = cmts_parser.parse_string(text, parse_all=True)
+        res = parser.parse_string(text, parse_all=True)
     except pp.ParseException as pe:
         print(pe.explain(depth=0)) # logging.warning
     styled_text = ''.join(flatten(add_comment(res.as_list())))
     styled_texts.append(styled_text)
+
     if FLAG_DEBUG:
         print('[original text]: ' + text)
         print('[parsed text]: ' + styled_text)
