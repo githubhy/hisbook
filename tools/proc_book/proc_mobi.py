5a30b514c864c7829d76560461075942505e94f9
Desktop <huangyea@desktop>
2022-06-17 23:30:59 +0800

    Test for building mobi books.

--- tools/proc_book/proc_mobi.py
+++ tools/proc_book/proc_mobi.py
@@ -40,7 +40,7 @@ if args.output_dir:
 else:
     out_dir = filepath
 
-out_filenames = [os.path.join(out_dir, '{0}-{1}'.format(os.path.basename(filepath), f) + '.md') for f in filenames]
+out_filenames = [os.path.join(out_dir, '{0}-{1}'.format(os.path.basename(filepath), f) + '.Rmd') for f in filenames]
 
 for i in range(len(in_filenames)):
     lines = []
