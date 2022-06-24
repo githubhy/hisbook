57640f4f344ba8bf8405278c6b8e7b9a73f3ea35
Desktop <huangyea@desktop>
2022-06-24 23:51:34 +0800

    Add () around _text to make the text match right. Don't know why.

--- tools/proc_book/proc_tags.py
+++ tools/proc_book/proc_tags.py
@@ -1,4 +1,3 @@
-from contextlib import suppress
 import logging
 import argparse
 from docx import Document
@@ -18,44 +17,23 @@ logging.basicConfig(level=logging.DEBUG,
     format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
     datefmt="%d/%b/%Y %H:%M:%S")
 
-# id: #xx-xx-xxx
-id_name_short = pp.Word(pp.alphanums + ppu.Chinese.alphanums)
-id_name = pp.Group(id_name_short + (pp.Literal('-') + id_name_short)[...]).setName('id_name')
-id = pp.Group(pp.Literal('#') + id_name).setName('id')
-
-# class: .xxx
-klass = pp.Literal('.') + pp.Regex(r'\w[\w\d-]*')
-
-def t(s, l, t):
-    return t[0]
-
-#all_chars = pp.printables + ppu.Chinese.printables + '〇　。，；：、﹑-！￥……（）―？《》〈〉〖〗【】「」■□♀．·“”„’‘◎○ ,;:/-!?<>~()[]{}#@$%^&*+=_/\\|`\'\'"\r\n\t\f\v'
 base_n_chars = '*[]{}()'
 _text_raw = pp.CharsNotIn(base_n_chars) #pp.Word(all_chars, excludeChars="[]{}()")
 _strong_emphasis = pp.QuotedString("**").set_name('strong_emphasis')
 _emphasis = pp.QuotedString("*").set_name('emphasis')
-_text = _strong_emphasis | _emphasis | _text_raw
+# The parentheses is a must. Don't know why.
+_text = (_strong_emphasis | _emphasis | _text_raw)
 
 # Nested expressions that have multiple opener/closer types: https://stackoverflow.com/a/4802004
-enclosed = pp.Forward()
-#sym_not_followd_by = ~(pp.Literal('{#') | pp.Literal('(#') | pp.Literal('{.'))
-# nestedParens = pp.nestedExpr(~pp.Literal(']') + pp.Literal('(') + ~pp.one_of('# .'),
-#                                 pp.Literal(')'),
-#                                 content=enclosed,
-#                                 ignore_expr=None)
-# nestedBrackets = pp.nestedExpr(pp.Literal('['),
-#                                 pp.Literal(']') + ~pp.one_of('{# (# {.'),
-#                                 content=enclosed,
-#                                 ignore_expr=None)
-# nestedCurlies = pp.nestedExpr(~pp.Literal(']') + pp.Literal('{') + ~pp.one_of('# .'),
-#                                 pp.Literal('}'),
-#                                 content=enclosed,
-#                                 ignore_expr=None)
-nestedParens = ~pp.Literal(']') + pp.Literal('(') + ~pp.one_of('# .') + enclosed + pp.Literal(')')
-nestedBrackets = pp.Literal('[') + enclosed + pp.Literal(']') + ~pp.one_of('{# (# {.')
-nestedCurlies = ~pp.Literal(']') + pp.Literal('{') + ~pp.one_of('# .') + enclosed + pp.Literal('}')
-enclosed << (_text | nestedParens | nestedBrackets | nestedCurlies)[1, ...]
-text = enclosed
+# Here I don't use pp.nested_expr() because it automatically suppresses the openers and closers
+nestedParens = pp.Forward()
+nestedBrackets = pp.Forward()
+nestedCurlies = pp.Forward()
+enclosed = (_text | nestedParens | nestedBrackets | nestedCurlies)[1, ...]
+nestedParens << ~pp.Literal(']') + pp.Literal('(') + ~pp.one_of('# .') + enclosed + pp.Literal(')')
+nestedBrackets << pp.Literal('[') + enclosed + pp.Literal(']') + ~pp.one_of('{# (# {.')
+nestedCurlies << ~pp.Literal(']') + pp.Literal('{') + ~pp.one_of('# .') + enclosed + pp.Literal('}')
+text = pp.Group(enclosed).set_name('text')
 
 _id_tag = pp.Literal('#').suppress() + pp.CharsNotIn(base_n_chars+'#. ').set_name('id_tag')
 _class_tag = pp.Literal('.').suppress() + pp.Word(pp.alphanums+'_-').set_name('class_tag')
@@ -64,7 +42,7 @@ _tag = pp.OneOrMore(_id_tag | _class_tag)
 _empty_id = pp.Group(pp.Literal('[]{').suppress() + _tag + pp.Literal('}').suppress()).set_name('empty_id')
 _title = pp.Group(
             pp.OneOrMore(pp.Literal('#')).set_parse_action(lambda toks: len(toks[0])).set_name('level')
-            + text.set_parse_action(lambda toks: toks[0].strip())
+            + pp.CharsNotIn('#{}').set_parse_action(lambda toks: toks[0].strip()).set_name('name')
             + pp.Opt(pp.Literal('{').suppress() + _tag + pp.Literal('}').suppress())
         ).set_name('title')
 _ref = pp.Group(pp.Literal('[').suppress() + text
@@ -74,12 +52,12 @@ _nested_class = pp.Forward()
 content = (_title | _empty_id | _ref | text | _nested_class)[1, ...]
 # pp.nested_expr() will make the opener and closer disappear, use the method mentioned here instead:
 # https://stackoverflow.com/a/23854320
-_nested_class << pp.Literal('[').suppress() + content + pp.Literal(']{').suppress() + _tag + pp.Literal('}').suppress()
+_nested_class << pp.Group(pp.Literal('[').suppress() + content
+                + pp.Literal(']{').suppress() + _tag + pp.Literal('}').suppress()
+                ).set_name('class')
 content.set_name('content')
 pp.enable_all_warnings()
 
-print(enclosed.parse_string('wo[我们[看到]了{真的{吗}}]'))
-print(enclosed.parse_string('见*吴*敏'))
 
 result = content.parse_string('''
 # 我们 看到了 wded {#jk-89 .chae_ei}
@@ -87,13 +65,14 @@ result = content.parse_string('''
     1. 塔里[]{#宋轶二十八-徐处仁-1}
     见*吴*敏[2](#宋轶二十八-吴敏-2)。
     2. []{#宋轶二十八-徐处仁-2}
-    见**吴**敏[4](#宋轶二十八-吴敏-4)。
-    3. [我们[看到]了]
+    见**吴yi**敏[4](#宋轶二十八-吴敏-4)。
+    3. wo[我们[看到]了{真的{吗}}]
     4. [我到了]{#我峨嵋-9 .e_8-9}
     ''')
 
 # print(''.join(flatten(result.as_list())))
-print(result)
+# Use res.dump() to analyze the results, see: https://stackoverflow.com/a/9996844
+print(result.dump())
 
 # fn = FilenameInOut(args.file_name, dir_out=args.output_dir, ext_out='.Rmd')
 # doc = Document(fn.get_in_names()[0])
